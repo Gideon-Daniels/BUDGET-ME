b7d8691d6d75ea671b7a5bc7061cf1ab8ee155b9
Gideon Daniels <gideon.daniels@safetyio.com>
2025-08-31 23:26:53 +0200

    created routes

--- backend/src/database/db.ts
+++ backend/src/database/db.ts
@@ -1,15 +1,40 @@
 import mysql from 'mysql2/promise';
+import { Report } from '../models/Reports.ts';
 
-export async function dbConnection(): Promise<mysql.Connection | Error> {
-  return mysql
-    .createConnection({
-      host: 'localhost',
-      user: 'root',
-      password: 'a',
-      database: 'budget_me_app',
-    })
-    .catch((e): Error => {
-      console.log(e);
-      return e;
-    });
+export class DatabaseService {
+  dbConnection!: mysql.Connection | Error;
+
+  constructor() {
+    this.start().then();
+  }
+
+  async start() {
+    this.dbConnection = await mysql
+      .createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'budget_me_app',
+      })
+      .catch((e): Error => {
+        console.log(e);
+        return e;
+      });
+  }
+
+  async createReport(data: Report) {
+    if (this.dbConnection instanceof Error) return;
+
+    const sql =
+      'INSERT INTO `reports`(`amount`,`type`,`category`,`date`,`description`) VALUES (?,?,?,?,?)';
+
+    const query = await this.dbConnection.execute(sql, [
+      data.amount,
+      data.type,
+      data.category,
+      data.date,
+      data.description,
+    ]);
+    console.log(query);
+  }
 }
--- backend/src/models/Reports.ts
+++ backend/src/models/Reports.ts
@@ -1,9 +1,7 @@
 export interface Report {
-  id: number;
   amount: number;
   type: 'income' | 'expense';
   category: string;
   date: string; // ISO format (e.g., "2025-08-30")
   description?: string;
-  created_at: Date;
 }
